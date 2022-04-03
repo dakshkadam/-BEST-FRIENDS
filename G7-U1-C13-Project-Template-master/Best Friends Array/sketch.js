db2fa0cd660c6103a2ec8fe1258b09b7e108fac6
dakshkadam <101383790+dakshkadam@users.noreply.github.com>
2022-04-03 10:58:26 +0530

    Add files via upload

---
+++ G7-U1-C13-Project-Template-master/Best Friends Array/sketch.js
@@ -0,0 +1,19 @@
+//1. Declare and initialize an array named friends
+var friends =["Daksh","Shivam","Shivanshu","Dhan","Tej"];
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  background("white");
+  console.log("My best friends are :");
+
+  //2. print the names in friends array in the console:
+  for (i=0;i<friends.length;i++){
+    console.log(friends[i]);
+  }
+}
+
+function draw() {
+    
+}
+
+    
+
